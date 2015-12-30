c05c3066d5bf84ac39317bde3196f788f66e9876
codezealer <codezealer@gmail.com>
2015-12-30 22:45:29 +0800

    Initial commit

---
+++ stone/src/me/zhl/stone/Test.java
@@ -0,0 +1,11 @@
+package me.zhl.stone;
+
+/**
+ * Created by zhl on 12/30/15.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello,Stone.");
+    }
+}
