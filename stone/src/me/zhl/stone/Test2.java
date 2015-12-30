9d11ad09b001f3f393dbc44770aeda8ab235916c
codezealer <codezealer@gmail.com>
2015-12-30 22:48:26 +0800

    add Test2

---
+++ stone/src/me/zhl/stone/Test2.java
@@ -0,0 +1,12 @@
+package me.zhl.stone;
+
+/**
+ * Created by zhl on 12/30/15.
+ */
+public class Test2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Another hello world.");
+    }
+}
